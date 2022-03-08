f96ac88d4c135cec9c53c2a2b61655ceec3cd47b
sebas moran <sebasmorandez@hotmail.com>
2022-03-08 14:16:54 -0600

    Fixed PRINT3 & 4 macros

--- defs.h
+++ defs.h
@@ -6,11 +6,9 @@
 
 #define PRINT1(fmt, x1) PR(fmt, x1), NL
 #define PRINT2(fmt, x1, x2) PR(fmt, x1), PRINT1(fmt, x2)
-#define PRINT3(fmt, x1, x2, x3) PR(fmt, x1) PRINT2(fmt, x2, x3)
-#define PRINT4(fmt, x1, x2, x3, x4) PR(fmt, x1) \
-PRINT3(fmt, x2, x3, x4)
-#define PRINT5(fmt, x1, x2, x3, x4, x5) PR(fmt, x1) \
-PRINT4(fmt, x2, x3, x4, x5)
+#define PRINT3(fmt, x1, x2, x3) PR(fmt, x1), PRINT2(fmt, x2, x3)
+#define PRINT4(fmt, x1, x2, x3, x4) PR(fmt, x1), PRINT3(fmt, x2, x3, x4)
+#define PRINT5(fmt, x1, x2, x3, x4, x5) PR(fmt, x1), PRINT4(fmt, x2, x3, x4, x5)
 
 int arreglo[15];
 
