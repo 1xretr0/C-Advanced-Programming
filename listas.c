1535a8b2f239605f87364fd6594ec66fa4a42218
sebas moran <sebasmorandez@hotmail.com>
2022-03-15 14:21:30 -0600

    created new pointers exercise

---
+++ listas.c
@@ -0,0 +1,12 @@
+// apuntadores
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int costo;  // variable de tipo entero
+    int *apuntador; // apuntador a variable de tipo entero
+
+    // system("pause");
+    return 0;
+}
