bc07f3a433d81c1b2af51370b43c3a634934ae45
sebas moran <sebasmorandez@hotmail.com>
2022-03-23 14:44:48 -0600

    worked on exercise

--- listas.c
+++ listas.c
@@ -45,11 +45,34 @@ int main(){
     ptrArreglo[2] = &arreglo[2];
     printf("la direccion de memoria del ptrArreglo es %p y hay %d\n", ptrArreglo[2], *ptrArreglo[2]);*/
 
-    porValor(costo);
+    /* porValor(costo);
     printf("Costo = %d\n", costo);
 
     porReferencia(&costo);
-    printf("Costo = %d\n", costo);
+    printf("Costo = %d\n", costo);*/
+
+    printf("la direccion del arreglo[0] es %p\n", &arreglo[0]);
+    printf("la direccion del arreglo[1] es %p\n", &arreglo[1]);
+    printf("la direccion del arreglo[2] es %p\n", &arreglo[2]);
+    printf("la direccion del arreglo[3] es %p\n", &arreglo[3]);
+    printf("la direccion del arreglo[4] es %p\n\n", &arreglo[4]);
+
+    pointer = &arreglo[2];
+    printf("la direccion guardada en pointer - 2 es: %p\n", pointer - 2);
+    printf("la direccion guardada en pointer - 1 es: %p\n", pointer - 1);
+    printf("la direccion guardada en pointer es: %p\n", pointer);
+    printf("la direccion guardada en pointer + 1 es: %p\n", pointer + 1);
+    printf("la direccion guardada en pointer + 2 es: %p\n", pointer + 2);
+
+    // OFFSET no se mueve apuntador, solo referencia direccion
+    *(pointer +2) = 10;
+
+    // apuntador si se mueve y cambia valor
+    pointer = &arreglo[4];
+    *pointer = 10;
+
+    pointer += 2;
+    *pointer = 10;
     // system("pause");
     return 0;
 }
