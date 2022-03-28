5d8b94adcd852daec25867b83689063455b4da59
sebas moran <sebasmorandez@hotmail.com>
2022-03-28 14:22:01 -0600

    updated exercise as teacher xd

--- listas.c
+++ listas.c
@@ -1,78 +1,61 @@
-// pointers
+#include <stdio.h>  // Para las funciones de i/o
+#include <stdlib.h> // Para el usoi de system()
 
-#include <stdio.h>
-#include <stdlib.h>
+void porValor(int variable);
+void porReferencia(int *variable);
 
-void porValor(int variable){
+void porValor(int variable)
+{
     variable += 5;
     printf("Variable = %d\n", variable);
 }
 
-void porReferencia(int *variable){
+void porReferencia(int *variable)
+{
     *variable += 5;
     printf("Variable = %d\n", *variable);
 }
 
-int main(){
-    int costo = 60;  // variable de tipo entero
-    int *pointer; // pointer a variable de tipo entero
-    int arreglo[5] = {1,2,3,4,5};
-    int *ptrArreglo[5];
+int main()
+{
+    int costo = 60;                   // Declaro una variable de tipo entero llamada costo
+    int *pointer = NULL;              // Declaro un ap.untador a enteros llamado pointer
+    int arreglo[5] = {0, 1, 2, 3, 4}; // declaro un arreglo de 5 enteros
+    int *ptrArreglo[5];               // declaro un arreglo de 5 apuntadores a entero
 
-    /*costo = 60;
-    printf("la direccion de memoria de costo es %p y hay %i\n", &costo, costo);
+    printf("La direccion de memoria de costo es: %p y hay %d\n", &costo, costo);
 
     pointer = &costo;
-    printf("la direccion de memoria de pointer es %p y hay %p\n", &pointer, pointer);
+    printf("La direccion de memoria de pointer es: %p y hay %p\n", &pointer, pointer);
 
-    printf("El contenido (*) de pointer es %i\n", *pointer);
+    printf("El contenido (*) de pointer es %d\n", *pointer);
 
     *pointer = 50;
-    printf("El contenido (*) de pointer es %i\n", *pointer);
+    printf("La direccion de memoria de costo es: %p y hay %d\n", &costo, costo);
 
-    printf("El contenido de costo es %i\n", costo);
+    ////====================================================================
+    // printf("La direccion del arreglo[0] es %p\n", &arreglo[0]);
+    // printf("La direccion del arreglo[1] es %p\n", &arreglo[1]);
+    // printf("La direccion del arreglo[2] es %p\n", &arreglo[2]);
+    // printf("La direccion del arreglo[3] es %p\n", &arreglo[3]);
+    // printf("La direccion del arreglo[4] es %p\n", &arreglo[4]);
 
-    // arreglos
-    printf("la direccion del arreglo[0] es %p\n", &arreglo[0]);
-    printf("la direccion del arreglo[1] es %p\n", &arreglo[1]);
-    printf("la direccion del arreglo[2] es %p\n", &arreglo[2]);
-    printf("la direccion del arreglo[3] es %p\n", &arreglo[3]);
-    printf("la direccion del arreglo[4] es %p\n", &arreglo[4]);
+    ////------------------- apuntadores a arreglos----------
+    // pointer = arreglo;
+    // printf("La direccion de memoria de pointer es: %p y hay %d\n", pointer, *pointer);
 
-    pointer = &arreglo[2];
-    printf("la direccion de memoria del pointer es %p y hay %d\n", pointer, *pointer);
+    //// ------------------ arreglos de apuntadores -----------
 
-    ptrArreglo[2] = &arreglo[2];
-    printf("la direccion de memoria del ptrArreglo es %p y hay %d\n", ptrArreglo[2], *ptrArreglo[2]);*/
+    // ptrArreglo[2] = &arreglo[2];
+    // printf("La direccion de memoria de ptrArreglo[2] es: %p y hay %d\n", ptrArreglo[2], *ptrArreglo[2]);
 
-    /* porValor(costo);
+    porValor(costo);
+    printf("Costo = %d\n", costo);
+    porReferencia(&costo);
     printf("Costo = %d\n", costo);
 
-    porReferencia(&costo);
-    printf("Costo = %d\n", costo);*/
+    // scanf("%d", &costo)
 
-    printf("la direccion del arreglo[0] es %p\n", &arreglo[0]);
-    printf("la direccion del arreglo[1] es %p\n", &arreglo[1]);
-    printf("la direccion del arreglo[2] es %p\n", &arreglo[2]);
-    printf("la direccion del arreglo[3] es %p\n", &arreglo[3]);
-    printf("la direccion del arreglo[4] es %p\n\n", &arreglo[4]);
-
-    pointer = &arreglo[2];
-    printf("la direccion guardada en pointer - 2 es: %p\n", pointer - 2);
-    printf("la direccion guardada en pointer - 1 es: %p\n", pointer - 1);
-    printf("la direccion guardada en pointer es: %p\n", pointer);
-    printf("la direccion guardada en pointer + 1 es: %p\n", pointer + 1);
-    printf("la direccion guardada en pointer + 2 es: %p\n", pointer + 2);
-
-    // OFFSET no se mueve apuntador, solo referencia direccion
-    *(pointer +2) = 10;
-
-    // apuntador si se mueve y cambia valor
-    pointer = &arreglo[4];
-    *pointer = 10;
-
-    pointer += 2;
-    *pointer = 10;
-    // system("pause");
+    system("pause");
     return 0;
 }
