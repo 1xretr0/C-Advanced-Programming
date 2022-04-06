2e36e80d29a954b38e825269954f8aa1b3602f73
Sebas Moran <65722993+its-Retr0@users.noreply.github.com>
2022-04-06 14:21:28 -0500

    updated exercise. still working on else block.

--- listas.c
+++ listas.c
@@ -2,11 +2,37 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+void insert();
+
 struct Nodo{
     char info;
     struct Nodo *sig;
 };
+typedef struct Nodo nodo;
+
+nodo *raiz = NULL;  // lista vacia
 
 int main(){
+    int costo;
+    nodo cajaZapato;
+
     return 0;
+}
+
+void insert(char dato){
+    nodo *nuevo = NULL;
+    nuevo = (nodo *) malloc(sizeof(nodo));
+    if (nuevo == NULL){
+        printf("No hay memoria suficiente\n");
+        exit(1);
+    }
+
+    nuevo -> info = dato;
+    nuevo -> sig = NULL;
+    if (raiz == NULL){
+        raiz = nuevo;
+    }
+    else{
+        // no
+    }
 }
