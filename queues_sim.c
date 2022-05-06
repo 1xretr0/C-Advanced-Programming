86b815042c8b80929ebac90c1239ee860653211a
Retr0 <sebasmorandez@hotmail.com>
2022-05-06 13:20:35 -0500

    started to work on project input code part is working fine and is validated

--- queues_sim.c
+++ queues_sim.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
-#include <queues.h>
+#include <stdlib.h>
+#include <ctype.h>
+// #include <queues.h>
 
-int main(void){
-    
+int main(void)
+{
+    // variables iniciales de entrada
+    int r_llegada, n_cajeros, r_cajeros[8];
+
+    // input inicial. faltan valids
+    printf("**ENTRADA**\n\n");
+    printf("Razon de llegada Promedio: ");
+    do{
+        scanf("%i", &r_llegada);
+        if (r_llegada > 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("Ingrese una razon valida: ");
+        }
+    }while (r_llegada <= 0);
+
+    printf("Numero de cajeros: ");
+    do
+    {
+        scanf("%i", &n_cajeros);
+        if (n_cajeros > 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("Ingrese un numero valido: ");
+        }
+    } while (n_cajeros <= 0);
+
+
+    // ciclo input razon de cada cajero
+    for (int i = 0; i < n_cajeros; i++){
+        printf("Razon de atencion del cajero %i: ", i+1);
+        scanf("%i", &r_cajeros[i]);
+    }
+    system("cls");
 }
